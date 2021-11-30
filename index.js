9d59a32787fcd3678bc62495c5929eb0e2b2b3e3
rdeepak2002 <rdeepak2002@gmail.com>
2021-11-30 16:40:39 -0500

    updated server response and gitignore

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Wavelength!')
+  res.send('Hello World from Wavelength! If you are on a Verizon network, make a request to http://155.146.4.228. Otherwise, make a request to http://3.82.14.59.')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
