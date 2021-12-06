b4a5fb2b80a35003ffa922b98fcdeb52e97d9a6c
rdeepak2002 <rdeepak2002@gmail.com>
2021-12-05 19:37:57 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = (parseInt(process.env.PORT) || 5000)
 
+// http server setup
 app.use(cors({
     origin: "*"
 }))
 
 app.get('/', (req, res) => {
   console.log('sent response')
-  res.send('Hello World from Wavelength! If you are on a Verizon network, make a request to http://155.146.4.228. Otherwise, make a request to http://18.232.126.27.')
+  res.send('If you are on a Verizon network, please make a request to http://155.146.4.228')
 })
 
+// create socket and http servers
 let WSServer = require('ws').Server;
 let server = require('http').createServer();
 
@@ -20,57 +22,39 @@ let wss = new WSServer({
   server: server
 });
 
-// Also mount the app here
-server.on('request', app);
-
 wss.on('connection', function connection (ws) {
-
   console.log('client connected')
 
   ws.on('message', (message) => {
-    // ws.send(JSON.stringify({
-    //   answer: 42
-    // }));
-
-    // console.log(`received message`);
-
-    let messageObj;
-
     try {
-      messageObj = JSON.parse(message);
+      // get the message sent in json format
+      const messageObj = JSON.parse(message);
 
-      // console.log(messageObj.base64)
-
-      // send camera stream to all clients
+      // send camera stream data to all clients
       if(messageObj.sender === 'camera') {
         const dataObj = {
           'sender': 'camera',
           'base64': `${messageObj.base64}`
-        }
+        };
 
         const dataStr = JSON.stringify(dataObj);
 
-        // ws.send('hi')
-        // console.log('sending', dataStr);
-        //
-        // console.log('sending hi')
-        // ws.send(JSON.stringify({
-        //   answer: 43
-        // }));
         wss.clients.forEach(function each(client) {
           client.send(dataStr);
         });
-        // ws.send(dataStr);
       }
     }
     catch(e) {
+      // message is not in a valid json format
       console.log('invalid message', message);
     }
   });
 });
 
+// mount the http server app
+server.on('request', app);
 
+// make server listen on the port specified
 server.listen(port, function() {
-
   console.log(`http/ws server listening on ${port}`);
 });
